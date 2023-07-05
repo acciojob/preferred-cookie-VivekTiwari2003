06665060ec5085c445d24b08e252d5b9161ad833
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-07-05 13:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+window.onload = function() {
+  var form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var fontSize = document.getElementById('fontsize').value;
+    var fontColor = document.getElementById('fontcolor').value;
+
+    // Store user preferences using localStorage
+    localStorage.setItem('fontSize', fontSize);
+    localStorage.setItem('fontColor', fontColor);
+
+    applyUserPreferences();
+  });
+
+  // Function to apply user preferences
+  function applyUserPreferences() {
+    var fontSize = localStorage.getItem('fontSize');
+    var fontColor = localStorage.getItem('fontColor');
+
+    if (fontSize) {
+      document.body.style.fontSize = fontSize + 'px';
+    }
+
+    if (fontColor) {
+      document.body.style.color = fontColor;
+    }
+  }
+
+  // Apply user preferences on page load
+  applyUserPreferences();
+};
